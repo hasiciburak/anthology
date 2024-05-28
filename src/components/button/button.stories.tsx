858b2fee07c1ef2831c88111510e3b254ee70ced
Burak Haşıcı <burakhasici@gmail.com>
2024-05-28 18:16:12 +0300

    Added destructive variant and argtypes

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -7,6 +7,12 @@ const meta = {
   component: Button,
   args: {
     children: 'Button',
+    disabled: false,
+  },
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+    },
   },
 } satisfies Meta;
 
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,17 +1,17 @@
 import { ComponentProps } from 'react';
 import styles from './button.module.css';
+import clsx from 'clsx';
+
 /* 
  This means that
  the button component takes everything that button elements take,
  plus a variant prop.
  */
 export type ButtonProps = ComponentProps<'button'> & {
-  variant: 'primary' | 'secondary' | 'destructive';
+  variant?: 'primary' | 'secondary' | 'destructive';
 };
 
-export const Button = ({ variant, ...props }: ButtonProps) => {
-  let className = styles.button;
-  if (variant === 'secondary') className += ' ' + styles.secondary;
-  if (variant === 'destructive') className += ' ' + styles.destructive;
-  return <button {...props} className={className} />;
+export const Button = ({ variant = 'primary', className, ...props }: ButtonProps) => {
+  let classes = clsx(styles.button, styles[variant], className);
+  return <button {...props} className={classes} />;
 };
